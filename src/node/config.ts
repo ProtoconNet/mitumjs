82f901cc3672d0799fba1682d45ed028e15d5588
HayoungOh5 <145317641+HayoungOh5@users.noreply.github.com>
2023-11-27 11:42:23 +0900

    fix: remove controllers config from STO config (#22)

--- src/node/config.ts
+++ src/node/config.ts
@@ -91,7 +91,6 @@ export const Config = {
 	},
 	STO: {
 		PARTITION: getRangeConfig(3, 10),
-		ADDRESS_IN_CONTROLLERS: getRangeConfig(0, 10),
 	},
 	DAO: {
 		ADDRESS_IN_WHITELIST: getRangeConfig(0, 10),
