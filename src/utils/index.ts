8c38f1ab37cf2c0c2f45504e22bbd2129aecffb8
오하영 <alice0l02@naver.com>
2024-05-02 11:48:05 +0900

    add: typeGuard for ErrorResponse, SuccessResponse

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,10 +2,12 @@ import { SortFunc } from "./sort"
 import { hasOverlappingAddress } from "./duplicate"
 import { sha256, sha3, keccak256 } from "./hash"
 import { delegateUri, apiPathWithParams, apiPathWithHashParams, apiPathWithParamsExt } from "./apiPathUtils"
+import { isSuccessResponse, isErrorResponse } from "./typeGuard"
 
 export {
     sha256, sha3, keccak256,
     SortFunc,
     hasOverlappingAddress,
     delegateUri, apiPathWithParams, apiPathWithHashParams, apiPathWithParamsExt,
+    isSuccessResponse, isErrorResponse
 }
--- src/utils/typeGuard.ts
+++ src/utils/typeGuard.ts
@@ -1,5 +1,14 @@
 import { Operation as OP, Fact } from "../operation/base"
+import { ErrorResponse, SuccessResponse } from "../types" 
 
 export const isOpFact = (operation: any): operation is OP<Fact> => {
     return operation instanceof OP;
+}
+
+export const isErrorResponse = (response: ErrorResponse | SuccessResponse): response is ErrorResponse => {
+    return 'error_code' in response;
+}
+
+export const isSuccessResponse = (response: ErrorResponse | SuccessResponse): response is SuccessResponse => {
+    return 'data' in response;
 }
