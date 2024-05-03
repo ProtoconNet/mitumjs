6445545436aa8616957322ea57178e1a51c8a3cf
오하영 <alice0l02@naver.com>
2024-05-03 09:53:38 +0900

    refactor: add jsDocs

--- src/node/index.ts
+++ src/node/index.ts
@@ -16,6 +16,17 @@ export class Node extends Generator {
         super("", api, delegateIP)
     }
 
+    /**
+     * Get information about the nodes in the network.
+     * @async
+     * @returns The `data` of `SuccessResponse` represents an array of information of nodes:
+     * - `network_id`: The ID of the network to which the node belongs.
+     * - `last_manifest`: Information about the most recently created block.
+     * - `network_policy`: Policy information of the network.
+     * - `local`: Information about the local node.
+     * - `consensus`: Status of consensus on the node.
+     * - `_hint`: Indicates that the data represents node information.
+     */
     async getNodeInfo() {
         Assert.check(
             this.api !== undefined || this.api !== null,
@@ -30,6 +41,22 @@ export class Block extends Generator {
         super("", api, delegateIP)
     }
 
+    /**
+     * Get the account information for the given public key.
+     * @async
+     * @param {number} [limit] - (Optional) The maximum number of items to retrieve.
+     * @param {number} [offset] - (Optional) The number of items skip before starting to return data.
+     * @param {boolean} [reverse] - (Optional) Whether to return the items in reverse newest order.
+     * @returns The `data` of `SuccessResponse` is a array with block manifest info object:
+     * - `_hint`: Indicates mitum engine version,
+     * - `_embedded`:
+     * - - `Manifest`: manifest info includes `proposed_at`, `states_tree`, `hash`, `previous`, `proposal`, `operations_tree`, `suffrage`, `_hint`, `height`. </div/>
+     * - - `operations`: number of operations included in the block,
+     * - - `confirmed_at`: timestamp,
+     * - - `proposer`: node name of proposer,
+     * - - `round`: number of round to manifest,
+     * - `_links`: links to get additional information
+     */
     async getAllBlocks(limit?: number, offset?: number, reverse?: true) {
         Assert.check(
             this.api !== undefined || this.api !== null,
@@ -38,6 +65,27 @@ export class Block extends Generator {
         return await getAPIData(() => block.getBlocks(this.api, this.delegateIP, limit, offset, reverse))
     }
 
+
+    /**
+     * Get information of a block by hash.
+     * @async
+     * @param {string} [hash] - The hash value of the block to retrieve.
+     * @returns The `data` of `SuccessResponse` represents a block manifest:
+     * - `Manifest`:
+     * - - `proposed_at`: Timestamp when the block was proposed, 
+     * - - `states_tree`: Hash for state tree,
+     * - - `hash`: Hash for the block, 
+     * - - `previous`: Hash for the previous block, 
+     * - - `proposal`: Hash for the proposal, 
+     * - - `operations_tree`: Hash for the operation tree, 
+     * - - `suffrage`: Hash for the suffrage, 
+     * - - `_hint`: Hint for the manifest,
+     * - - `height`: Block height
+     * - `operations`: The number of operations included in the block,
+     * - `confirmed_at`: Timestamp when the block was confirmed,
+     * - `proposer`: The node name of proposer,
+     * - `round`: The number of round to manifest
+     */
     async getBlockByHash(hash: string) {
         Assert.check(
             this.api !== undefined || this.api !== null,
@@ -46,6 +94,26 @@ export class Block extends Generator {
         return await getAPIData(() => block.getBlockByHash(this.api, hash, this.delegateIP))
     }
 
+    /**
+     * Get information of a block by height.
+     * @async
+     * @param {number | string} [height] - The height of the block to retrieve.
+     * @returns The `data` of `SuccessResponse` represents a block manifest:
+     * - `Manifest`: 
+     * - - `proposed_at`: Timestamp when the block was proposed, 
+     * - - `states_tree`: Hash for state tree,
+     * - - `hash`: Hash for the block, 
+     * - - `previous`: Hash for the previous block, 
+     * - - `proposal`: Hash for the proposal, 
+     * - - `operations_tree`: Hash for the operation tree, 
+     * - - `suffrage`: Hash for the suffrage, 
+     * - - `_hint`: Hint for the manifest,
+     * - - `height`: Block height
+     * - `operations`: The number of operations included in the block,
+     * - `confirmed_at`: Timestamp when the block was confirmed,
+     * - `proposer`: The node name of proposer,
+     * - `round`: The number of round to manifest
+     */
     async getBlockByHeight(height: number | string) {
         Assert.check(
             this.api !== undefined || this.api !== null,
@@ -62,6 +130,27 @@ export class Block extends Generator {
     //     return await getAPIData(() => operation.getBlockOperationsByHash(this.api, hash, this.delegateIP))
     // }
 
+    
+    /**
+     * Get all operations contained in a block of given height.
+     * @async
+     * @param {number | string} [height] - The height of the block to retrieve operations from.
+     * @param {number} [limit] - (Optional) The maximum number of items to retrieve.
+     * @param {number} [offset] - (Optional) The number of items skip before starting to return data.
+     * @param {boolean} [reverse] - (Optional) Whether to return the items in reverse newest order.
+     * @returns The `data` of `SuccessResponse` represents an array of all operations in the block:
+     * - `_hint`: Indicates mitum engine version,
+     * - `_embedded`:
+     * - - `_hint`: Hint for the operation,
+     * - - `hash`: Hash for the fact,
+     * - - `operation`: Information of the operation includes `hash`, `fact`, `signs`, `_hint`,
+     * - - `height`: Block height containing the operation,
+     * - - `confirmed_at`: Timestamp when the block was confirmed,
+     * - - `reason`: Reason for operation failure,
+     * - - `in_state`: Boolean indicating whether the operation was successful or not,
+     * - - `index`: Index of the operation in the block
+     * - `_links`: Links to get additional information
+     */
     async getOperationsByHeight(height: number | string,limit?: number, offset?: number, reverse?: true) {
         Assert.check(
             this.api !== undefined || this.api !== null,
