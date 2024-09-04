97809cccb5e07561f45b285711879b786d0df4af
오하영 <alice0l02@naver.com>
2024-09-04 11:16:36 +0900

    feat: add prescription hint

---
+++ src/alias/hint/prescription.ts
@@ -0,0 +1,14 @@
+export default {
+    REGISTER_MODEL: {
+        FACT: "mitum-prescription-register-model-operation-fact",
+        OPERATION: "mitum-prescription-register-model-operation",
+    },
+    REGISTER_PRESCRIPTION: {
+        FACT: "mitum-prescription-register-prescription-operation-fact",
+        OPERATION: "mitum-prescription-register-prescription-operation",
+    },
+    USE_PRESCRIPTION: {
+        FACT: "mitum-prescription-use-prescription-operation-fact",
+        OPERATION: "mitum-prescription-use-prescription-operation",
+    },
+}
