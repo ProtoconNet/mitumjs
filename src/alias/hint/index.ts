e5e852e532d2983bb491034e02532978bb3ed96b
오하영 <alice0l02@naver.com>
2024-09-20 14:32:19 +0900

    rm other contract hint

--- src/alias/hint/index.ts
+++ src/alias/hint/index.ts
@@ -1,24 +1,8 @@
 import CURRENCY from "./currency"
-import NFT from "./nft"
-import CREDENTIAL from "./credential"
-import DAO from "./dao"
-import KYC from "./kyc"
-import STO from "./sto"
-import TIMESTAMP from "./timestamp"
-import TOKEN from "./token"
-import POINT from "./point"
 import STORAGE from "./storage"
 
 export default {
     FACT_SIGN: "base-fact-sign",
     CURRENCY,
-    NFT,
-    CREDENTIAL,
-    DAO,
-    KYC,
-    STO,
-    TIMESTAMP,
-    TOKEN,
-    POINT,
     STORAGE,
 }
