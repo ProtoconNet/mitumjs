431f0f16affeff325d03942a56cdd58e0e9473c2
오하영 <alice0l02@naver.com>
2024-11-06 14:43:13 +0900

    feat: add hint for AA did

--- src/alias/hint/did.ts
+++ src/alias/hint/did.ts
@@ -19,5 +19,17 @@ export default {
     REACTIVATE_DID: {
         FACT: "mitum-did-reactivate-did-operation-fact",
         OPERATION: "mitum-did-reactivate-did-operation",
-    }
+    },
+    UPDATE_DID_DOCUMENT: {
+        FACT: "mitum-did-update-did-document-operation-fact",
+        OPERATION: "mitum-did-update-did-document-operation",
+    },
+    DATA: "mitum-did-data",
+    RESOURCE: "mitum-did-resource",
+    DOCUMENT: "mitum-did-document",
+    AUTHENTICATION: {
+        ASYMMETRIC_KEY: "mitum-did-asymmetric-key-authentication",
+        SOCIAL_LOGIN: "mitum-did-social-login-authentication",
+    },
+    VERIFICATION_METHOD:"mitum-did-verification-method-authentication"
 }
