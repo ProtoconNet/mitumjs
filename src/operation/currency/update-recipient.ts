b064f8eb6dc3a87a0ca0efd45c00ec025cd3cc16
오하영 <alice0l02@naver.com>
2024-11-27 11:24:26 +0900

    feat: add updateRecipient method

---
+++ src/operation/currency/update-recipient.ts
@@ -0,0 +1,62 @@
+import { Fact, FactJson } from "../base"
+
+import { HINT } from "../../alias"
+import { Address } from "../../key"
+import { SortFunc, hasOverlappingAddress } from "../../utils"
+import { CurrencyID } from "../../common"
+import { Assert, ECODE, MitumError } from "../../error"
+
+export class UpdateRecipientFact extends Fact {
+    readonly sender: Address
+    readonly contract: Address
+    readonly recipients: Address[]
+    readonly currency: CurrencyID
+
+    constructor(
+        token: string,
+        sender: string | Address,
+        contract: string | Address,
+        currency: string | CurrencyID,
+        recipients: (string | Address)[],
+    ) {
+        super(HINT.CURRENCY.UPDATE_RECIPIENT.FACT, token)
+        this.sender = Address.from(sender)
+        this.contract = Address.from(contract)
+        this.currency = CurrencyID.from(currency)
+        this.recipients = recipients.map(a => Address.from(a))
+        this._hash = this.hashing()
+        
+        // Assert.check(
+        //     (this.recipients.length !== 0),
+        //     MitumError.detail(ECODE.INVALID_FACT, "empty recipients"),
+        // )
+        Assert.check(
+            hasOverlappingAddress(this.recipients),
+            MitumError.detail(ECODE.INVALID_FACT, "duplicate address found in recipients"),
+        )
+    }
+
+    toBuffer(): Buffer {
+        return Buffer.concat([
+            super.toBuffer(),
+            this.sender.toBuffer(),
+            this.contract.toBuffer(),
+            this.currency.toBuffer(),
+            Buffer.concat(this.recipients.sort(SortFunc).map(a => a.toBuffer())),
+        ])
+    }
+
+    toHintedObject(): FactJson {
+        return {
+            ...super.toHintedObject(),
+            sender: this.sender.toString(),
+            contract: this.contract.toString(),
+            currency: this.currency.toString(),
+            recipients: this.recipients.sort(SortFunc).map((w) => w.toString()),
+        }
+    }
+
+    get operationHint() {
+        return HINT.CURRENCY.UPDATE_RECIPIENT.OPERATION
+    }
+}
