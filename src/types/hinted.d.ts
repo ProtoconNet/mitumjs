979189eb350d360f2959498a63cd0d3ee37f08d9
오하영 <alice0l02@naver.com>
2024-12-17 14:21:51 +0900

    feat: add HintedExtensionObject

--- src/types/hinted.d.ts
+++ src/types/hinted.d.ts
@@ -2,3 +2,9 @@ export type HintedObject = {
 	_hint?: string,
 	[i: string]: any
 }
+
+export type HintedExtensionObject = {
+	authentication: HintedObject,
+	proxy_payer?: HintedObject,
+	settlement: HintedObject
+}
