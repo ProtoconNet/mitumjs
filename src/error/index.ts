bc3c99f2d383635b5f51bd4e3f267589d7c31d3c
오하영 <alice0l02@naver.com>
2025-01-13 10:07:47 +0900

    feat: add new assert class for array type

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,10 +1,11 @@
 import { ECODE, DCODE, PCODE, assignCodeFromErrorMessage } from "./code"
-import { MitumError, Assert, StringAssert } from "./assert"
+import { MitumError, Assert, StringAssert, ArrayAssert } from "./assert"
 
 export {
     ECODE, DCODE, PCODE,
     MitumError,
     Assert,
     StringAssert,
+    ArrayAssert,
     assignCodeFromErrorMessage,
 } 
