5660d8896139bb434569c4b51f42325aff36159d
오하영 <alice0l02@naver.com>
2025-01-13 10:12:31 +0900

    fix: remove export hasOverlappingAddress

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 import { SortFunc } from "./sort"
-import { hasOverlappingAddress } from "./duplicate"
 import { sha256, sha3, keccak256, getChecksum } from "./hash"
 import { delegateUri, apiPathWithParams, apiPathWithHashParams, apiPathWithParamsExt } from "./apiPathUtils"
 import { isSuccessResponse, isErrorResponse } from "./typeGuard"
@@ -7,7 +6,6 @@ import { isSuccessResponse, isErrorResponse } from "./typeGuard"
 export {
     sha256, sha3, keccak256, getChecksum,
     SortFunc,
-    hasOverlappingAddress,
     delegateUri, apiPathWithParams, apiPathWithHashParams, apiPathWithParamsExt,
     isSuccessResponse, isErrorResponse
 }
