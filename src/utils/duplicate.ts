f6028d563be4367b850249c9a6c0931f9009d6a2
오하영 <alice0l02@naver.com>
2025-01-15 16:33:55 +0900

    rm: duplicate.ts

--- src/utils/duplicate.ts
+++
@@ -1,5 +0,0 @@
-import { Address } from "../key";
-
-export const hasOverlappingAddress = (arr: (string | Address)[]) => (
-    new Set(arr.map(a => a instanceof Address ? a.toString() : a)).size == arr.length
-)
