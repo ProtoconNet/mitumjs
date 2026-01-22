f911c41712621b4de8f4ba946eafbbe82aa15d53
오하영 <alice0l02@naver.com>
2026-01-22 14:53:15 +0900

    fix: add new static methods for Hint class

--- src/common/hint.ts
+++ src/common/hint.ts
@@ -2,13 +2,28 @@ import { Version } from "../node"
 import { IString } from "../types"
 
 export class Hint implements IString {
-    private s: string
+    private s: string;
 
     constructor(s: string) {
-        this.s = s
+        this.s = Hint.hasVersion(s) ? s : `${s}-${Version.get()}`;
     }
 
     toString(): string {
-        return `${this.s}-${Version.get()}`
+        return this.s;
+    }
+
+    static hasVersion(s: string): boolean {
+        const suffix = `-${Version.get()}`;
+        return s.endsWith(suffix);
+    }
+
+    static fromString(s: string): Hint {
+        if (!Hint.hasVersion(s)) {
+            throw new Error(`Invalid hinted string (missing version): ${s}`);
+        }
+
+        const h = Object.create(Hint.prototype) as Hint;
+        h.s = s;
+        return h;
     }
 }
