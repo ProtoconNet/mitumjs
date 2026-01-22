6fc79389f15a3a451f666841682892704f6118d5
오하영 <alice0l02@naver.com>
2026-01-22 14:49:52 +0900

    feat: add new hint for AUTHENTICATION

--- src/alias/hint/authdid.ts
+++ src/alias/hint/authdid.ts
@@ -7,19 +7,11 @@ export default {
         FACT: "mitum-did-create-did-operation-fact",
         OPERATION: "mitum-did-create-did-operation",
     },
-    MIGRATE_DID: {
-        ITEM: "mitum-did-migrate-did-item",
-        FACT: "mitum-did-migrate-did-operation-fact",
-        OPERATION: "mitum-did-migrate-did-operation",
-    },
     UPDATE_DID_DOCUMENT: {
         FACT: "mitum-did-update-did-document-operation-fact",
         OPERATION: "mitum-did-update-did-document-operation",
     },
     DOCUMENT: "mitum-did-document",
-    AUTHENTICATION: {
-        ASYMMETRIC_KEY: "mitum-did-asymmetric-key-authentication",
-        SOCIAL_LOGIN: "mitum-did-social-login-authentication",
-    },
+    AUTHENTICATION: "mitum-did-verification-method",
     VERIFICATION_METHOD:"mitum-did-verification-method-authentication"
 }
